6f4fc7ca5ac0bd73e3ea91990b2039dfb6f95eed
shreya singh <shreyasingh10022001@gmail.com>
2019-08-30 11:44:54 +0530

    shreya branch

---
+++ app/src/main/java/org/lol/wazirbuild/msilib/recyclerFirebase.java
@@ -0,0 +1,14 @@
+package org.lol.wazirbuild.msilib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recyclerFirebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_firebase);
+    }
+}
